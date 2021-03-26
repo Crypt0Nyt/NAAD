a25cdfa3724efb5d84fed2fe69fe54a330a8e2db
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-03-26 20:54:06 +0530

    Input and Output

---
+++ src/Tasks0/J01.java
@@ -0,0 +1,16 @@
+package Tasks0;
+
+import java.util.Scanner;
+
+public class J01 {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        int number = reader.nextInt();
+
+        System.out.println("You entered: " + number);
+    }
+}
+
