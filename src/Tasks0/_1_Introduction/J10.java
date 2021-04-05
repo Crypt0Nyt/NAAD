41cf65ed2385913247c8e4f422be3a4fee3eb19b
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-04-05 22:07:53 +0530

    Roots of the quadratic equations

--- src/Tasks0/_1_Introduction/J10.java
+++ src/Tasks0/_1_Introduction/J10.java
@@ -1,7 +1,30 @@
 package Tasks0._1_Introduction;
 
+//Program to find the roots of the quadratic equations
+
 public class J10 {
     public static void main(String[] args) {
+        //a, b, c are the coefficients of a quadratic equation
+        double a = 1, b = -4, c = 4;
+        double D = (b * b) - (4 * a * c);
 
+        //if roots are real and distinct or equal
+        if (D >= 0)
+        {
+            double root1 = (-b + Math.sqrt(D)) / (2 * a);
+            double root2 = (-b - Math.sqrt(D)) / (2 * a);
+            System.out.println("Roots of the equation are: " + root1 + " and " + root2);
+        }
+
+        //if roots are imaginary
+        else
+        {
+            double realPart = -b / (2 * a);
+            double imgPart = (Math.sqrt(D)) / (2 * a);
+            System.out.println("Roots of the equation are: " + realPart + "+" + imgPart + "i and " + realPart + "+" + imgPart + "i");
+        }
     }
+
+
 }
+
