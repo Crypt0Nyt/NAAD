e1363cb5c9b844e3af81841b679613ea48dff7f3
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-04-05 22:12:08 +0530

    Round a Number

---
+++ src/Tasks0/_1_Introduction/J12.java
@@ -0,0 +1,12 @@
+package Tasks0._1_Introduction;
+
+//Program to Round a Number to n Decimal Places
+
+public class J12 {
+    public static void main(String[] args) {
+        double num = 3.142857;
+
+        System.out.format("%.3f", num);
+    }
+}
+
