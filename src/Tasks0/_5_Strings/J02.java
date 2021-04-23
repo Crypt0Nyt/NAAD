c17af8def1b5eb6f37234509aec5d6e90ff04a23
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-04-23 17:37:52 +0530

    Compare Strings

---
+++ src/Tasks0/_5_Strings/J02.java
@@ -0,0 +1,17 @@
+package Tasks0._5_Strings;
+
+//Program to Compare Strings
+
+public class J02 {
+    public static void main(String[] args) {
+
+        String str1 = "apple";
+        String str2 = "apple";
+
+        if(str1 == str2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+
+}
