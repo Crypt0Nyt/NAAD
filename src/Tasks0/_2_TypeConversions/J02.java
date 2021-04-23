b10f937be3eb7f3bdc3652be7be9a5234cf3ba17
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-04-23 17:34:34 +0530

    convert int type variables to String

---
+++ src/Tasks0/_2_TypeConversions/J02.java
@@ -0,0 +1,18 @@
+package Tasks0._2_TypeConversions;
+
+//Program to convert int type variables to String
+
+public class J02 {
+    public static void main(String[] args) {
+
+        int num1 = 372;
+        int num2 = 549201;
+
+        String str1 = Integer.toString(num1);
+        String str2 = Integer.toString(num2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+    }
+}
