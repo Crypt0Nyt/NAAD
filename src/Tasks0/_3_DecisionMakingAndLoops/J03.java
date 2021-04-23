ceccd29d9023eb5f7469fe32ca46c62dcd532c8d
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-04-23 17:35:15 +0530

    Display Alphabets (A to Z) using loop

---
+++ src/Tasks0/_3_DecisionMakingAndLoops/J03.java
@@ -0,0 +1,12 @@
+package Tasks0._3_DecisionMakingAndLoops;
+
+//Program to Display Alphabets (A to Z) using loop
+
+public class J03 {
+    public static void main(String[] args) {
+        char ch;
+
+        for(ch = 'A'; ch <= 'Z'; ++ch)
+            System.out.print(ch + " ");
+    }
+}
